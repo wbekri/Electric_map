1b506dae8031b7015595214b8c7d64aabbc5a2b2
II7II <ibrhmdee@gmail.com>
2021-03-08 13:11:34 +0100

    Diaw a ajouter le formulaire Bornes

---
+++ database_bornes.php
@@ -0,0 +1,65 @@
+
+		<?php
+			
+			$vd = $_POST["Ville de depart"];
+			echo "vd";
+			
+			$va = $_POST["Ville d'arrivée"];
+			echo "va";
+			
+			$v = $_POST["Ville"];
+			echo "v";
+			
+			$c1 = $_POST["Criteres 1"];
+			echo "c1";
+			
+			$c2 = $_POST["Criteres 2"];
+			echo "c2";
+			
+			$c3 = $_POST["Criteres 3"];
+			echo "c3";
+			
+			$r = $_POST["Rayon"];
+			echo "r";
+			
+		?>
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
