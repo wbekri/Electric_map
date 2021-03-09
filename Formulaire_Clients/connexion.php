a82279539d991dfbcab013679578f441dc3e1f72
wbekri <wiambekri.h@gmail.com>
2021-03-10 00:54:06 +0100

    html a l'inscription

--- Formulaire_Clients/connexion.php
+++ Formulaire_Clients/connexion.php
@@ -6,20 +6,30 @@
    </head>
    <body>
 
+ <style>
+  *{
+  background-color: darkseagreen
+  }
+  #adresse{ text-align: center;
+      color: white;
+      padding-top: 50px;
+
+  }
+ </style>
+
 
 <form action="connecter.php" method="post" autocomplete="off">
-<p>
+<p id="adresse">
 	Adresse e-mail:
 	<input type="text" name="mail" value=""/> </p>
-    <p>
+    <p id="adresse">
     Mot de passe :
 	<input type="password" name="mdp1" value=""/> </p>
-	<p>
+	<p id="adresse">
 	<input type="submit" value="Envoyer">
 	</br>
 	</p> 
-
-	<a href="nouveauClient.php">Je veux creer un compte
+	<a id="adresse" href="nouveauClient.php">Je veux creer un compte
       </a>
       </br>
       
