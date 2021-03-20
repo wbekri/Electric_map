0fb98960c25e2d4df937fa3eacf68a13829b4898
idiarras <diarrassoubaibrahim44@gmail.com>
2021-03-20 16:45:13 +0100

    ibrahima le code ne marche pas sur mon ordi

--- Comparateur/formulaire_comparateur.php
+++ Comparateur/formulaire_comparateur.php
@@ -10,7 +10,8 @@
 		<div style="width : 100%; margin-top: 50px;">
 			<form method="post" action="recherche_comparateur.php" style="margin-left: auto;margin-right: auto;width: 200px;" >
 				
-				<p> Marque : <input type="text" name="marque" value=""/> </p>
+				<span >Marque de la voiture : </span><br>
+			<input style="width: 100%;" type="text" name="marque" placeholder="Ex :"><br><br>
 				
 				<input type="submit" value="recherche">
 				
--- Comparateur/recherche_comparateur.php
+++ Comparateur/recherche_comparateur.php
@@ -2,12 +2,12 @@
 <?php
 		
 		$bdd = new PDO('mysql:host=localhost;dbname=voitureelectrique;charset=utf8','root', '');
-		$rep = $bdd->query("SELECT * FROM voitureselectriques WHERE marque LIKE '".$_POST['marque']."'");
+		$rep = $bdd->query("SELECT * FROM `table 1`  WHERE marque LIKE '".$_POST['marque']."'");
 		
 		if ($rep) 
 		{
 			$nbrep = $rep ->rowCount();  
-			
+		
 			if ($nbrep > 0)  
 			{
 				echo "<p><h3>Voici les resultats de votre recherche :</h3></p>";
@@ -15,7 +15,7 @@
 					echo "<table border='1'>\n";
 					echo "<tr>\n";	
 					echo "<td><strong>Marque</strong></td>\n";				
-					echo "<td><strong>Modèle</strong></td>\n";
+					echo "<td><strong>Modele</strong></td>\n";
 					echo "<td><strong>Prix</strong></td>\n";
 					echo "<td><strong>Autonomie</strong></td>\n";
 					echo "<td><strong>Puissance</strong></td>\n";
@@ -28,11 +28,11 @@
 					echo "<tr>\n";
 					echo "<td>".$rech['marque']."</td>\n";
 					echo "<td>".$rech['modele']."</td>\n";
-					echo "<td>".$rech['montant']."</td>\n";
+					/*echo "<td>".$rech['prix']."</td>\n";
 					echo "<td>".$rech['autonomie']."</td>\n";
 					echo "<td>".$rech['puissance']."</td>\n";
 					echo "<td>".$rech['recharge']."</td>\n";
-					echo "<td>".$rech['consommation']."</td\n>";
+					echo "<td>".$rech['consommation']."</td\n>";*/
 					echo "<tr>\n";
 				}
 				
