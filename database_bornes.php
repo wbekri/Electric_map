e1b2f1fc4b0df96adb6d5fa3a4a84417d19fd737
idiarras <diarrassoubaibrahim44@gmail.com>
2021-03-29 16:18:59 +0200

    mise a jour

--- database_bornes.php
+++ database_bornes.php
@@ -12,22 +12,6 @@
 			echo '<br/>';
 			
 			
-			echo "Ville :";
-			echo $_POST['v'];
-			echo '<br/>';
-			
-			echo "Critere 1 :";
-			echo $_POST['c1'];
-			echo '<br/>';
-			
-			echo "Critere 2 :";
-			echo $_POST['c2'];
-			echo '<br/>';
-			
-			echo "Rayon :";
-			echo $_POST['r'];
-			echo '<br/>';
-			
 		?>
 	<body>
 		
--- recherche_bornes.php
+++ recherche_bornes.php
@@ -11,52 +11,14 @@
 		<p>Trouvez des bornes de recharge pour votre voiture électrique dans tout le territoire français !</p>
 	
 		<form action="database_bornes.php" method="post" autocomplete="off">
-		
-		
-				
-				<title>Recherche Simple</title>
+		<title>Recherche Simple</title>
 				<p>
 				Ville de depart:
-				<input type="text" name="vd" value=""/> </p>
+				<input type="text" name="vd" value=""placeholder="facultatif"/> </p>
 				<p>
 				Ville d'arrivee:
 				<input type="text" name="va" value="" placeholder="facultatif"/> </p>
-				
 				<p><input type="submit" value="Recherche"></p>	
-
-			
-			<p></p> <!--saut de ligne (à modifier)-->
-			
-			<fieldset class="position">
-			
-				<legend>Recherche Avancée</legend>
-				
-				<label for="v">Ville</label>
-				<input type="text" name="v" value=""/> </p>
-				
-				<label for="c1">Criteres 1</label>
-				<input type="text" name="c1" value=""/> </p>
-				
-				<label for="c2">Criteres 2</label>
-				<input type="text" name="c2" value=""/> </p>
-				
-				<label for="c3">Criteres 3</label>
-				<input type="text" id="c3" name="Criteres 3" value=""/> </p>
-			
-				<label for="r">Rayon</label>
-				<select id="r" name="Rayon"> 
-					<option value="moins de 25 km"> moins de 25 km</option>
-					<option value="25-50 km">25-50 km</option>
-					<option value="50-100 km">50-100 km</option>
-					<option value="plus de 100 km">plus de 100 km</option>
-				</select>
-				
-				<p><input type="submit" value="Recherche"></p>
-			</fieldset>
-		
-		
-		
-			
    </body>
    
 </html>
